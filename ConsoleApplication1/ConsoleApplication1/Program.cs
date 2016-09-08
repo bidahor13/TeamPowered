4a6412c4dc90962aacb33d3907f4a402fabc028a
Tunde Idahor <bidahor13@wou.edu>
2016-09-08 10:24:37 -0700

    Add a new method to lower.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,15 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+
+//My project
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        double runningTotal = 0;
+        bool keepGoing = true;
+        //Prompts the user for data entry
+
+        while (keepGoing)
         {
+            System.Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+
+            //Reads the users input
+            string entry = Console.ReadLine();
+
+            if (entry.ToLower() == "quit")
+            {
+                keepGoing = false;
+            }
+            else {
+
+                //Deals with the System.FormatException in case a string value is placed as an input value.
+                try
+                {
+                    double minutes = int.Parse(entry);
+                    if (minutes <= 0)
+                    {
+                        Console.WriteLine("The value " + minutes + " Minutes is not an acceptable value.");
+                        continue;
+                    }
+
+                    if (minutes <= 10)
+                    {
+                        Console.WriteLine("Better than nothing, am I right?");
+                    }
+                    else if (minutes <= 30)
+                    {
+                        Console.WriteLine("Way to go hot stuff!! ");
+                    }
+                    else if (minutes <= 60)
+                    {
+                        Console.WriteLine("You must be a ninja warrior in training");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ok!now you are just showing off!");
+                    }
+
+                    //Add minutes exercised as a total
+                    runningTotal += minutes;
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("This is not a valid input entry");
+                    continue;
+                }
+
+                //Display total minutes exercised to the screen
+                Console.Write("You have entered: " + runningTotal + " minutes" + "\n");
+
+            }
+            //Console.WriteLine("Your FINAL DATA: " + runningTotal + " minutes" );
+            //Console.WriteLine("Goodbye!"); 
         }
+
+        Console.WriteLine("Your FINAL DATA: " + runningTotal + " minutes");
+        Console.WriteLine("Goodbye!");
+
     }
 }
+
+
+
