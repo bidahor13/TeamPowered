2844597775dfc6ec016f887daa3d6de231bb8d9b
Tunde Idahor <bidahor13@wou.edu>
2016-09-08 01:10:22 -0700

    add a output statement

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This is a test class with my git account");
         }
     }
 }
